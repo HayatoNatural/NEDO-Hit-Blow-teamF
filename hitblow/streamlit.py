64ac255969707a7b4ae5fdd7d739daddb82b75c8
HayatoNatural <k1036laphyen19pc@gmail.com>
2021-10-10 21:34:17 +0900

    練習用

--- hitblow/streamlit.py
+++ hitblow/streamlit.py
@@ -3,16 +3,30 @@ import requests
 import time
 from typing import Any
 from PIL import Image
+from streamlit.legacy_caching.caching import cache
 from class_play_game import Playgame
 
-st.write('Count = ' + st.session_state.count)
 st.title("Welcome to Hit&Blow Game!")
 col1, col2, col3 = st.columns(3)
 col2.write("よろしくね！")
-col2.image("f007.png",use_column_width=True)
-col3.image("花火大会_gifmagazine.gif",use_column_width=True)
+
+
+st.title('Counter Example using Callbacks with args')
+if 'count' not in st.session_state:
+    st.session_state.count = 0
+
+@st.cache(allow_output_mutation=True)
+def cache_lst():
+    lst = []
+    return lst
+lst = []
+input = st.text_input('何か入力して下さい')
+if input:
+    lst.append(input)
+st.table(lst)
 
 url = "https://damp-earth-70561.herokuapp.com"
+
 
 def get_room(session:requests.Session,room_id) -> Any:
     url_get_room = url + "/rooms/" + str(room_id)
@@ -20,16 +34,16 @@ def get_room(session:requests.Session,room_id) -> Any:
     return result.json()
 
 
-def main():
-    session = requests.Session()
-    st.title('Counter Example')
-    if 'count' not in st.session_state:
-        st.session_state.count = 0
-    increment = st.button('Increment')
-    if increment:
-        st.session_state.count += 1
-    st.write('Count = ', st.session_state.count)
+# def main():
+#     session = requests.Session()
+#     st.title('Counter Example')
+#     if 'count' not in st.session_state:
+#         st.session_state.count = 0
+#     increment = st.button('Increment')
+#     if increment:
+#         st.session_state.count += 1
+#     st.write('Count = ', st.session_state.count)
 
 
-if __name__ =="__main__":
-    main()
+# if __name__ =="__main__":
+#     main()
