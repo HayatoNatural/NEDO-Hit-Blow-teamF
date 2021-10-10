fe135899fcdd828e40b5396aa12177ebb5ab6d99
HayatoNatural <k1036laphyen19pc@gmail.com>
2021-10-10 21:32:20 +0900

    アルゴリズム練習用

--- hitblow/Numberguess3.py
+++ hitblow/Numberguess3.py
@@ -1,5 +1,4 @@
 import random
-from typing import List,Tuple
 import itertools
 import argparse
 
@@ -10,8 +9,8 @@ class Numberguess2:
         self.count = 0
         self.history = []
         self.list_num_place = []
+        self.list_possible_ans_combination = []
         self.list_possible_ans = []
-        self.list_ans_combination = []
         if ans is not None:
             self.ans = ans
         else:
@@ -36,7 +35,7 @@ class Numberguess2:
                     self.blow += 1
 
 
-    def _first_three_times(self) -> None:
+    def _first_3_times(self) -> None:
         search_list = ["01234","56789","abcde"]
         for i in range(3):
             print("{}回目の入力です".format(self.count+1))
@@ -51,30 +50,75 @@ class Numberguess2:
                 print("!! 正解です !!")
                 break
 
-    def _make_list_possible_ans(self) -> None:
+    def _first_2_times(self) -> None:
+        search_list = ["01234","56789"]
+        for i in range(2):
+            print("{}回目の入力です".format(self.count+1))
+            self.num = search_list[i]
+            self.history.append(self.num)
+            self.count += 1
+            self._check_hit_blow(self.num,self.ans)
+            self.list_num_place.append(self.hit+self.blow)
+            print("-----",self.num)
+            print("!!  {} Hit, {} Blow  !!".format(self.hit,self.blow))
+            if self.hit == self.digits:
+                print("!! 正解です !!")
+                break
+
+
+    def _make_list_possible_ans_combination(self) -> None:
         for i in itertools.combinations("01234", self.list_num_place[0]):
             for j in itertools.combinations("56789", self.list_num_place[1]):
                 for k in itertools.combinations("abcde", self.list_num_place[2]):
                     for l in itertools.combinations("f", self.digits-sum(self.list_num_place)):
-                        m = "".join(i+j+k+l)
-                        list_1 = list(m)
-                        for n in itertools.permutations(list_1,5):
-                            o = "".join(n)
-                            self.list_possible_ans.append(o)
-        self._leave_possible_permutation()
+                        n = "".join(i+j+k+l)
+                        self.list_possible_ans_combination.append(n)
 
+    def _make_list_possible_ans_combination_2(self) -> None:
+        for i in itertools.combinations("01234", self.list_num_place[0]):
+            for j in itertools.combinations("56789", self.list_num_place[1]):
+                for k in itertools.combinations("abcdef", self.digits-sum(self.list_num_place)):
+                        m = "".join(i+j+k)
+                        self.list_possible_ans_combination.append(m)
 
-    def _leave_possible_permutation(self):
-        hit = self.hit
-        list_new_possible_ans = []
-        count = 1
-        for i in self.list_possible_ans:
+    def _remove_impossible_combination(self):
+        hb = self.hit + self.blow
+        for i in self.list_possible_ans_combination[:]:
             self._check_hit_blow(self.num,i)
-            # print("hit:{},self_hit:{}, selfnum:{}, k:{}, count:{}".format(hit,self.hit,self.num,i,count))
-            count += 1
-            if self.hit == hit:
-                list_new_possible_ans.append(i)
-        self.list_possible_ans = list_new_possible_ans
+            if self.hit + self.blow != hb:
+                self.list_possible_ans_combination.remove(i)
+
+    def _remove_impossible_permutation(self):
+        hit = self.hit
+        for i in self.list_possible_ans[:]:
+            self._check_hit_blow(self.num,i)
+            if self.hit != hit:
+                self.list_possible_ans.remove(i)
+
+    def _identify_number(self):
+        print("----------from first3 to 5C----------")
+        while True:
+            print("{}回目の入力です, 組み合わせの候補は{}通りです.".format(self.count+1,len(self.list_possible_ans_combination)))
+            self.num = random.choice(self.list_possible_ans_combination)
+            self.history.append(self.num)
+            self.count += 1
+            self._check_hit_blow(self.num,self.ans)
+            print("-----",self.num)
+            print("!!  {} Hit, {} Blow  !!".format(self.hit,self.blow))
+            if self.hit == self.digits:
+                print("正解です！")
+                break
+            elif self.hit + self.blow == self.digits:
+                self.list_ans_combination = [i for i in self.num]
+                for i in itertools.permutations(self.list_ans_combination,5):
+                    m = "".join(i)
+                    self.list_possible_ans.append(m)
+                print("----------from 5C to 5P----------")
+                self._remove_impossible_permutation()
+                self._identify_permutation()
+                break
+            else:
+                self._remove_impossible_combination()
 
 
     def _identify_permutation(self):
@@ -89,7 +133,7 @@ class Numberguess2:
             if self.hit == self.digits:
                 print("正解です！")
                 break
-            self._leave_possible_permutation()
+            self._remove_impossible_permutation()
 
 
     def _show_result(self) -> None:
@@ -107,9 +151,9 @@ class Numberguess2:
         print("------------------------")
 
     def _play_game_auto(self) -> None:
-        self._first_three_times()
-        self._make_list_possible_ans()
-        self._identify_permutation()
+        self._first_2_times()
+        self._make_list_possible_ans_combination_2()
+        self._identify_number()
         self._show_result()
 
 
@@ -130,12 +174,16 @@ def main() -> None:
     args = get_parser()
     ans= args.ans
 
-    if args.ans is not None:
-        runner = Numberguess2(ans=ans)
-    else:
-        runner = Numberguess2()
+    sum_count = 0
+    for _ in range(100):
+        if args.ans is not None:
+            runner = Numberguess2(ans=ans)
+        else:
+            runner = Numberguess2()
+        runner._play_game_auto()
+        sum_count += runner.count
+    print("100回の平均必要回答数 : {}".format(sum_count/100))
 
-    runner._play_game_auto()
 
 if __name__ == "__main__":
     main()
